2670b6c1568c87b0532d2a34dc2d353ec41bb95b
Ivi de Ferreira <84143486+IVI-FERREIRA@users.noreply.github.com>
2026-01-03 19:16:16 -0300

    Adcionar API de inferencia

--- src/api/main.py
+++ src/api/main.py
@@ -1,1 +1,36 @@
 
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+from pathlib import Path
+
+MODEL_PATH = Path("models/model.joblib")
+DATA_PATH = Path("data/processed/water.parquet")
+
+app = FastAPI(title="Water Anomaly API")
+
+model = joblib.load(MODEL_PATH)
+columns = pd.read_parquet(DATA_PATH).columns.tolist()
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.post("/predict")
+def predict(data: dict):
+    # valia se tods as colunas existem
+    if set(data.keys()) != set(map(str, columns)):
+        raise HTTPException(
+            status_code=400,
+            detail="JSON precisa conter todas as colunas do modelo"
+        )
+
+    df = pd.DataFrame([data])
+    df = df[columns] #garante ordem correta
+
+    pred = model.predict(df)[0]
+    result = "ANOMALIA" if pred == -1 else "NORMAL"
+    
+    return {"result": result}
