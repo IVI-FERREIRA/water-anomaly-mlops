577877e9a9d58b56bae48a6d9ef732c8539541cd
Ivi de Ferreira <84143486+IVI-FERREIRA@users.noreply.github.com>
2026-01-05 12:10:51 -0300

    Atualizar códigos main.py e infer.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,16 +1,9 @@
-
-from fastapi import FastAPI
-import joblib
-import pandas as pd
-from pathlib import Path
-
-MODEL_PATH = Path("models/model.joblib")
-DATA_PATH = Path("data/processed/water.parquet")
+from fastapi import FastAPI, HTTPException
+from src.infer import WaterAnomalyModel
 
 app = FastAPI(title="Water Anomaly API")
 
-model = joblib.load(MODEL_PATH)
-columns = pd.read_parquet(DATA_PATH).columns.tolist()
+model = WaterAnomalyModel()
 
 
 @app.get("/health")
@@ -20,17 +13,8 @@ def health():
 
 @app.post("/predict")
 def predict(data: dict):
-    # valia se tods as colunas existem
-    if set(data.keys()) != set(map(str, columns)):
-        raise HTTPException(
-            status_code=400,
-            detail="JSON precisa conter todas as colunas do modelo"
-        )
-
-    df = pd.DataFrame([data])
-    df = df[columns] #garante ordem correta
-
-    pred = model.predict(df)[0]
-    result = "ANOMALIA" if pred == -1 else "NORMAL"
-    
-    return {"result": result}
+    try:
+        result = model.predict(data)
+        return {"result": result}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- src/infer.py
+++ src/infer.py
@@ -1,1 +1,22 @@
+import joblib
+import pandas as pd
+from pathlib import Path
 
+MODEL_PATH = Path("models/model.joblib")
+DATA_PATH = Path("data/processed/water.parquet")
+
+
+class WaterAnomalyModel:
+    def __init__(self):
+        self.model = joblib.load(MODEL_PATH)
+        self.columns = pd.read_parquet(DATA_PATH).columns.tolist()
+
+    def predict(self, data: dict) -> str:
+        if set(data.keys()) != set(map(str, self.columns)):
+            raise ValueError("JSON precisa conter todas as colunas")
+
+        df = pd.DataFrame([data])
+        df = df[self.columns]
+
+        pred = self.model.predict(df)[0]
+        return "ANOMALIA" if pred == -1 else "NORMAL"
